bd363888286e5acd55345198d9fa38c0f502c5c2
Orlando Enrico <36771949+orlandoenrico@users.noreply.github.com>
2019-11-03 23:15:35 -0300

    Commit 4

--- src/Money.java
+++ src/Money.java
@@ -2,13 +2,24 @@ abstract class Money{
 
    	protected int amount;
 
+   	private String currency;
+
+   	String currency(){
+   		return currency;
+   	}
+
    	static Money dollar(int amount){
-   		return new Dollar(amount);
+   		return new Dollar(amount, "USD");
    	}
 
    	static Money franc(int amount){
-    	return new Franc(amount);
-    }
+    	return new Franc(amount, "CHF");
+   	}
+
+   	Money(int amount, String currency){
+   		this.amount = amount;
+   		this.currency = currency;
+   	}
 
    	abstract Money times(int multiplier);
 
@@ -16,4 +27,5 @@ abstract class Money{
     	Money money = (Money) object;
       	return amount == money.amount && getClass().equals(money.getClass());
    	}
+
 }
--- src/Tests.java
+++ src/Tests.java
@@ -33,4 +33,9 @@ public class Tests {
    		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 	}
 
+	public void testCurrency() {
+   		assertEquals("USD", Money.dollar(1).currency());
+   		assertEquals("CHF", Money.franc(1).currency());
+	}
+
 }
